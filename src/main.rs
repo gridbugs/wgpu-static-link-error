574cd70598aa43aeb19f5b33eac30e5fb8d19e1d
Stephen Sherratt <stephen@sherra.tt>
2019-11-27 02:46:45 +1100

    Uncomment

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
-    /*
     let adapter = wgpu::Adapter::request(&wgpu::RequestAdapterOptions {
         power_preference: wgpu::PowerPreference::Default,
         backends: wgpu::BackendBit::PRIMARY,
     });
-    println!("{:?}", adapter); */
+    println!("{:?}", adapter);
 }
