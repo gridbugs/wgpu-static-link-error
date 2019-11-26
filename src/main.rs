33c56ec72998fe7135a462e208a2a5c1ddafa2eb
Stephen Sherratt <stephen@sherra.tt>
2019-11-27 10:24:09 +1100

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    env_logger::init();
     let adapter = wgpu::Adapter::request(&wgpu::RequestAdapterOptions {
         power_preference: wgpu::PowerPreference::Default,
         backends: wgpu::BackendBit::DX12,
