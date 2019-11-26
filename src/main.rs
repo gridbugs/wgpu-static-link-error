cd383260ee9564b615a5996f1c34c8437cf30cd7
Stephen Sherratt <stephen@sherra.tt>
2019-11-27 01:04:15 +1100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let adapter = wgpu::Adapter::request(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::Default,
+        backends: wgpu::BackendBit::PRIMARY,
+    });
+    println!("{:?}", adapter);
+}
