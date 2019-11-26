ebe08096452b722f04220347aecd2a1614eb2c88
Stephen Sherratt <stephen@sherra.tt>
2019-11-27 09:47:11 +1100

    Use dx12 backend

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let adapter = wgpu::Adapter::request(&wgpu::RequestAdapterOptions {
         power_preference: wgpu::PowerPreference::Default,
-        backends: wgpu::BackendBit::PRIMARY,
+        backends: wgpu::BackendBit::DX12,
     });
     println!("{:?}", adapter);
 }
